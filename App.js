c8b8f349f48da02578d7b80f4ced07cc25117eb8
Himanshu12145 <himansu12145@gmail.com>
2023-03-13 22:43:20 +0530

    Added all the files to there...

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import Splash from './src/screens/auth/Splash';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Splash />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/screens/auth/Splash/index.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const Splash = () => {
+  return (
+    <View>
+      <Text>You'll Find All you need Here!</Text>
+    </View>
+  );
+};
+
+export default Splash;
